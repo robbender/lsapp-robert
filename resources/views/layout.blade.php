40839f73ae63fe62673ff02793971dea12a3da72
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-07-29 08:01:48 -0700

    Added nav bar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{config('app.name', 'LSAPP') }}</title>
 </head>
 <body>
     
