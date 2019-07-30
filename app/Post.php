cc8bc4253e6e2aa8a09440d88e81eca30efa32cc
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-07-30 07:35:19 -0700

    Updated Post model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //// protected $fillable = [
+    //     'title', 
+    //     'body', 
+    //     'id',
+    // ];
+
+    protected $guarded = [];
 }
