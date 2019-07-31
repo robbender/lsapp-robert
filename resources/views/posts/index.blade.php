7f12d6043aaf1e8509f0ca4efef0c9cb94668068
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-07-31 07:36:43 -0700

    Updated Post index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,6 @@
 
     <h1>Posts</h1>
     <br>
-    <br>
  @if(count($posts) > 1)
      
         @foreach($posts as $post)
@@ -17,10 +16,9 @@
             
 
         @endforeach
-        
+
     @else
         <p>No post found</p>
-    @endif
     
 @endif
 
