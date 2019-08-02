30a81d6ab0326c45d1b930c03bbaf18c5d0a4b92
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-02 08:25:01 -0700

    updated navbar and post controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -29,7 +29,8 @@ class PostsController extends Controller
      */
     public function create()
     {
-        return view('posts/create');
+        // dd('create');
+        return view('posts.create');
     }
 
     /**
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -22,7 +22,7 @@
               <a class="nav-link" href="/posts">Posts</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              <a class="nav-link" href="/posts/create" tabindex="-1" aria-disabled="true">Create Post</a>
             </li>
           </ul>
         </div>
