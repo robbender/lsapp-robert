b069df167d5476ee8f54ff08f3c7aa86a43895eb
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-02 07:26:53 -0700

    Created messages view

--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -7,3 +7,19 @@
         </div>
     @endforeach
 @endif
+
+@if(session('success'))
+        <div class="alert alert-success">
+
+            {{ session('success') }}
+            
+        </div>
+@endif
+
+@if(session('error'))
+        <div class="alert alert-danger">
+
+            {{ session('error') }}
+            
+        </div>
+@endif
