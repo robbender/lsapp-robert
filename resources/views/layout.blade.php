ae1b2b7eedd806dddee27a5d958c48ff96ba1445
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-06 08:07:13 -0700

    Added CKeditor

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -20,6 +20,11 @@
 
     </div>
 
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+
     <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
