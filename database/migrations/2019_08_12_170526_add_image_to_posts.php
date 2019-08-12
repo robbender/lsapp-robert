ee8f9e4141a0cc1c3d1401457c279aef338d5b0d
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-12 10:09:01 -0700

    Updated image migration

--- database/migrations/2019_08_12_170526_add_image_to_posts.php
+++ database/migrations/2019_08_12_170526_add_image_to_posts.php
@@ -26,7 +26,7 @@ class AddImageToPosts extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropColumn('image');
         });
     }
 }
