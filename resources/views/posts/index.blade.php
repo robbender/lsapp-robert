8d1378f77564f5ce92de06a9850be408135029e1
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-12 06:36:14 -0700

    updated index view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,8 @@
     <h1>Posts</h1>
     <br>
  @if(count($posts) > 0)
-     
+ {{-- @if ($posts->count()) --}}
+
         @foreach($posts as $post)
 
             <div class="well">
@@ -13,14 +14,14 @@
                 <small>Written on {{ $post->created_at }}</small>
             </div>
             <br>
-            
+
         @endforeach
         {{ $posts->links() }}
 
     @else
         <p>No post found</p>
-    
+
 @endif
 
-    
+
 @endsection
