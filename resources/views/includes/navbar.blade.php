c8f6457a96e71ea447600464a377bce516db19e0
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-12 06:14:28 -0700

    Updated Layout wBS CDNs

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
         <a class="navbar-brand" href="/">{{config('layout.name', 'LSAPP') }}</a>
-        <button class="navbar-toggler" 
-                type="button" 
-                data-toggle="collapse" 
-                data-target="#navbarNav" 
-                aria-controls="navbarNav" 
-                aria-expanded="false" 
+        <button class="navbar-toggle collapsed"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
                 aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
