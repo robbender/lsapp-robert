a4f77fe9ff9c9399d85d19bb54f3472f24512ba6
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-12 12:36:16 -0700

    Updated web routes

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,20 @@ Route::get('/posts/edit', 'PostsController@update');
 Route::get('/posts/create', 'PostsController@create');
 // Route::post('/posts', 'PostsController@store');
 
+Route::get('images/{filename}', function ($filename)
+{
+    $path = storage_path() . '/img/' . $filename;
+
+    if(!File::exists($path)) abort(404);
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
+
+
 Route::resource('posts', 'PostsController');
