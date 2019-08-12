d80e0de25f029aa9e3b87b5f860cba589a2a9fa7
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-08-12 07:34:42 -0700

    Created new image migration

--- database/migrations/2019_08_12_142114_add_image_to_posts.php
+++ database/migrations/2019_08_12_142114_add_image_to_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddImageToPosts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->string('image');
+        Schema::table('posts', function (Blueprint $table) {
+            //
         });
     }
 
@@ -25,8 +25,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->dropColumn('image');
+        Schema::table('posts', function (Blueprint $table) {
+            //
         });
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,16 +11,24 @@
         <div class="form-row">
           <div class="form-group col-md-6">
             <label for="">Title</label>
-            <input type="text" name="title" class="form-control" id="" placeholder="Title">
+            <input type="text" name="title" class="form-control" id="" placeholder="Title" required>
           </div>
         </div>
         <div class="form-group">
             <label for="">Body</label>
-            <textarea class="form-control rounded-0" name="body" rows="10" placeholder="Body Text"></textarea>
-          </div>
+            <textarea class="form-control rounded-0" name="body" rows="10" placeholder="Body Text" required></textarea>
+        </div>
+
+        <div class="field">
+            {{-- <label class="label is-large" type="text" name="image"></label> --}}
+            <input type="file" class="btn btn-info {{ $errors->has('image') ? 'is-danger' : '' }}" id="" name="image" accept="image/png, image/jpeg" enctype="multipart/form-data" placeholder="Image" required>
+
+        </div>
+        <br>
+
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
 
 
-    
+
 @endsection
